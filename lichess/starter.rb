a18dbbf126a776370d3236d003449af408b2e341
Karl Heitmann <khf1988@gmail.com>
2021-02-04 19:51:50 -0300

    example return

--- lichess/starter.rb
+++ lichess/starter.rb
@@ -5,13 +5,7 @@ require 'json'
 n = ARGV[0].to_i
 
 def getLiveStreamers(n)
-  url = URI("https://lichess.org/streamer/live")
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  request = Net::HTTP::Get.new(url)
-  response = http.request(request)
-  return JSON.parse(response.read_body)[1..10]
+  return JSON.parse(File.read("live_streamers.json"))[0..n]
 end
 
 def getUserData(id)
@@ -22,7 +16,7 @@ end
 
 # data = request("https://lichess.org/api/user/{username}")
 # live_streamers = getLiveStreamers(3)
-live_streamers = JSON.parse(File.read("live_streamers.json"))[0..n]
+live_streamers = getLiveStreamers(n)
 puts live_streamers
 
 live_streamers_full = live_streamers.map do |streamer|
