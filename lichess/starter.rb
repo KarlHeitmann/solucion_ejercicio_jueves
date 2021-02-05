1972e37f78d7de93081454ea0581a4639fd454ba
Karl Heitmann <khf1988@gmail.com>
2021-02-05 01:24:42 -0300

    colocado starter html

--- lichess/starter.rb
+++ lichess/starter.rb
@@ -56,8 +56,51 @@ puts
 puts "***********"
 # promedio_juegos = live_streamers_full.inject {|sumaout, streamer| sumaout += streamer["perfs"].reduce(0) {|suma_in, (perfs, val)| suma_in += val["rating"] } }
 # promedio_juegos = live_streamers_full.inject {|sumaout, streamer| sumaout += streamer["perfs"]["classical"]["rating"] }
-promedio_juegos = live_streamers_full["perfs"]["classical"].inject {|sumaout, streamer| sumaout += streamer["rating"].to_i }
+# promedio_juegos = live_streamers_full["perfs"]["classical"].inject {|sumaout, streamer| sumaout += streamer["rating"].to_i }
 print "+++++"
-print promedio_juegos
+# print promedio_juegos
 puts
 # Escribir en un archivo los datos personales. Poner los datos en una tabla con bootstrap.
+
+string_html = '''<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+    <title>PAGINA LICHESS ADL</title>
+  </head>
+  <body>
+    <h1>Pagina lichess ADL Costa Rica G 47</h1>
+    '''
+# TODO: Ponga su código acá:
+dato_tmp = "Hola"
+string_html += '<p>' + dato_tmp + '</p>'
+
+string_html += '''
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
+'''
+
+fHTML = File.open("final.html","w")
+fHTML.write(string_html)
+fHTML.close
+
+# 4. Tome los datos completos de los live_streamers_full,
+#
